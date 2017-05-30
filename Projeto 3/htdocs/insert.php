67636952391343b6ef73ae62c8e6672797d589e6
Gonçalo Silva <Goncaloaps@users.noreply.github.com>
2017-05-30 23:47:26 +0100

    Upload do update.php
    Criação do insert para as tabelas arcas, fornecedores, lojas e produtos

--- Projeto 3/htdocs/insert.php
+++ Projeto 3/htdocs/insert.php
@@ -1,23 +1,12 @@
 <html>
-    <body>
-        <?php
-            $A_ID = $_REQUEST['A_ID'];
-            $localizacao = $_REQUEST['localizacao'];
-            $capacidade = $_REQUEST['capacidade'];
-            try {
-                $host = "127.0.0.1";
-                $user ="root";
-                $password = "root";
-                $dbname = "proj3";
-                $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-                $sql = "INSERT INTO Arcas (A_ID,localizacao,capacidade) VALUES ('$A_ID', '$localizacao', '&capacidade');";
-                echo("<p>$sql</p>");
-                $db->query($sql);
-                $db = null;
-            }
-            catch (PDOException $e) {
-                echo("<p>ERROR: {$e->getMessage()}<p/>");
-            }
-        ?>
-    </body>
+<body>
+
+<p>Insert</p>
+<a href="insert/produto.php">Produto</a><br>
+<a href="insert/loja.php">Loja</a><br>
+<a href="insert/fornecedor.php">Fornecedor</a><br>
+<a href="insert/arca.php">Arca</a><br>
+<p><a href="/">Voltar</a></p>
+
+</body>
 </html>
---
+++ Projeto 3/htdocs/update.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<p>Insert</p>
+<a href="update/produto.php">Produto</a><br>
+<a href="update/loja.php">Loja</a><br>
+<a href="update/arca.php">Arca</a><br>
+<p><a href="/">Voltar</a></p>
+
+</body>
+</html>
