d317e052e5e78c5ee46eca455e4864506b567d85
Gonçalo Silva <Goncaloaps@users.noreply.github.com>
2017-05-30 23:48:19 +0100

    Criar pasta

---
+++ Projeto 3/htdocs/arca_done.php
@@ -0,0 +1,27 @@
+<html>
+    <body>
+        <p>Registo gravado com sucesso!</p>
+        <?php
+            $A_ID = $_POST["A_ID"];
+            $localizacao = $_POST["localizacao"];
+            $capacidade = $_POST["capacidade"];
+            
+            try {
+                $host = "127.0.0.1";
+                $user ="root";
+                $password = "root";
+                $dbname = "proj3";
+                $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = "INSERT INTO Arcas (A_ID,localizacao,capacidade) VALUES ($A_ID,'$localizacao',$capacidade);";
+                echo("<p>$sql</p>");
+                $db->query($sql);
+                $db = null;
+            }
+            catch (PDOException $e) {
+                echo("<p>ERROR: {$e->getMessage()}<p/>");
+            }
+            ?>
+        <p><a href="/">Voltar</a></p>
+    </body>
+</html>
