7a51eba619798b778873703e1a323c9b5a59087b
Gonçalo Silva <Goncaloaps@users.noreply.github.com>
2017-05-30 10:48:28 +0100

    Add CREATE and DROP DATABASE statements

--- Projeto 3/_proj3_.sql
+++ Projeto 3/_proj3_.sql
@@ -3,16 +3,24 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 28, 2017 at 05:26 PM
+-- Generation Time: May 30, 2017 at 11:45 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `proj3`
 --
+CREATE DATABASE IF NOT EXISTS `proj3` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `proj3`;
 
 -- --------------------------------------------------------
 
@@ -20,10 +28,30 @@ SET time_zone = "+00:00";
 -- Table structure for table `Arcas`
 --
 
+DROP TABLE IF EXISTS `Arcas`;
 CREATE TABLE `Arcas` (
   `A_ID` int(11) NOT NULL DEFAULT '0',
   `localizacao` varchar(255) DEFAULT NULL,
   `capacidade` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Arcas`
+--
+
+INSERT INTO `Arcas` (`A_ID`, `localizacao`, `capacidade`) VALUES
+(0, '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Congeladoras`
+--
+
+DROP TABLE IF EXISTS `Congeladoras`;
+CREATE TABLE `Congeladoras` (
+  `A_ID` int(11) DEFAULT NULL,
+  `temperatura` decimal(2,1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -32,6 +60,7 @@ CREATE TABLE `Arcas` (
 -- Table structure for table `Contrato`
 --
 
+DROP TABLE IF EXISTS `Contrato`;
 CREATE TABLE `Contrato` (
   `localizacao_servicos` varchar(255) DEFAULT NULL,
   `P_ID` int(11) DEFAULT NULL,
@@ -44,6 +73,7 @@ CREATE TABLE `Contrato` (
 -- Table structure for table `Datas`
 --
 
+DROP TABLE IF EXISTS `Datas`;
 CREATE TABLE `Datas` (
   `producao` date DEFAULT NULL,
   `expiracao` date DEFAULT NULL,
@@ -57,6 +87,7 @@ CREATE TABLE `Datas` (
 -- Table structure for table `Encomenda`
 --
 
+DROP TABLE IF EXISTS `Encomenda`;
 CREATE TABLE `Encomenda` (
   `quantidade_da_encomenda` int(11) DEFAULT NULL,
   `precos` decimal(4,2) DEFAULT NULL,
@@ -69,6 +100,7 @@ CREATE TABLE `Encomenda` (
 -- Table structure for table `Fornece`
 --
 
+DROP TABLE IF EXISTS `Fornece`;
 CREATE TABLE `Fornece` (
   `l_nome` varchar(255) DEFAULT NULL,
   `F_ID` int(11) DEFAULT NULL
@@ -80,6 +112,7 @@ CREATE TABLE `Fornece` (
 -- Table structure for table `Fornecedores`
 --
 
+DROP TABLE IF EXISTS `Fornecedores`;
 CREATE TABLE `Fornecedores` (
   `F_ID` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -87,9 +120,30 @@ CREATE TABLE `Fornecedores` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Frigorificas`
+--
+
+DROP TABLE IF EXISTS `Frigorificas`;
+CREATE TABLE `Frigorificas` (
+  `A_ID` int(11) DEFAULT NULL,
+  `temperatura` decimal(2,1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Frigorificas`
+--
+
+INSERT INTO `Frigorificas` (`A_ID`, `temperatura`) VALUES
+(NULL, '5.0'),
+(NULL, '5.0');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Loja`
 --
 
+DROP TABLE IF EXISTS `Loja`;
 CREATE TABLE `Loja` (
   `l_nome` varchar(50) NOT NULL,
   `horario` datetime DEFAULT NULL,
@@ -97,19 +151,13 @@ CREATE TABLE `Loja` (
   `funcionarios` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `Loja`
---
-
-INSERT INTO `Loja` (`l_nome`, `horario`, `localizacao`, `funcionarios`) VALUES
-('laranjeiras', NULL, 'Benfica', '10');
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Pertence`
 --
 
+DROP TABLE IF EXISTS `Pertence`;
 CREATE TABLE `Pertence` (
   `l_nome` varchar(255) DEFAULT NULL,
   `P_ID` int(11) DEFAULT NULL,
@@ -122,6 +170,7 @@ CREATE TABLE `Pertence` (
 -- Table structure for table `Produtos`
 --
 
+DROP TABLE IF EXISTS `Produtos`;
 CREATE TABLE `Produtos` (
   `P_ID` int(11) NOT NULL,
   `tipo` varchar(50) DEFAULT NULL,
@@ -131,19 +180,13 @@ CREATE TABLE `Produtos` (
   `quantidade_stock` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `Produtos`
---
-
-INSERT INTO `Produtos` (`P_ID`, `tipo`, `nome`, `stock_minimo`, `preco_venda`, `quantidade_stock`) VALUES
-(1, 'refrigerantes', 'Coca-cola', 5, '0.70', 50);
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Secções`
 --
 
+DROP TABLE IF EXISTS `Secções`;
 CREATE TABLE `Secções` (
   `s_nome` varchar(64) NOT NULL DEFAULT '',
   `localizacao` varchar(255) DEFAULT NULL,
@@ -156,6 +199,7 @@ CREATE TABLE `Secções` (
 -- Table structure for table `Tamanho`
 --
 
+DROP TABLE IF EXISTS `Tamanho`;
 CREATE TABLE `Tamanho` (
   `area` float DEFAULT NULL,
   `n_caixas` int(11) DEFAULT NULL,
@@ -172,6 +216,12 @@ CREATE TABLE `Tamanho` (
 --
 ALTER TABLE `Arcas`
   ADD PRIMARY KEY (`A_ID`);
+
+--
+-- Indexes for table `Congeladoras`
+--
+ALTER TABLE `Congeladoras`
+  ADD KEY `A_ID` (`A_ID`);
 
 --
 -- Indexes for table `Contrato`
@@ -204,6 +254,12 @@ ALTER TABLE `Fornece`
 --
 ALTER TABLE `Fornecedores`
   ADD PRIMARY KEY (`F_ID`);
+
+--
+-- Indexes for table `Frigorificas`
+--
+ALTER TABLE `Frigorificas`
+  ADD KEY `A_ID` (`A_ID`);
 
 --
 -- Indexes for table `Loja`
@@ -242,6 +298,12 @@ ALTER TABLE `Tamanho`
 --
 
 --
+-- Constraints for table `Congeladoras`
+--
+ALTER TABLE `Congeladoras`
+  ADD CONSTRAINT `congeladoras_ibfk_1` FOREIGN KEY (`A_ID`) REFERENCES `Arcas` (`A_ID`);
+
+--
 -- Constraints for table `Contrato`
 --
 ALTER TABLE `Contrato`
@@ -268,6 +330,12 @@ ALTER TABLE `Fornece`
   ADD CONSTRAINT `fornece_ibfk_2` FOREIGN KEY (`F_ID`) REFERENCES `Fornecedores` (`F_ID`);
 
 --
+-- Constraints for table `Frigorificas`
+--
+ALTER TABLE `Frigorificas`
+  ADD CONSTRAINT `frigorificas_ibfk_1` FOREIGN KEY (`A_ID`) REFERENCES `Arcas` (`A_ID`);
+
+--
 -- Constraints for table `Pertence`
 --
 ALTER TABLE `Pertence`
@@ -280,3 +348,7 @@ ALTER TABLE `Pertence`
 --
 ALTER TABLE `Tamanho`
   ADD CONSTRAINT `tamanho_ibfk_1` FOREIGN KEY (`l_nome`) REFERENCES `Loja` (`l_nome`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
